b5489da2cc588b2989b3d970ad6ad3080222f042
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-14 13:23:47 -0700

    Completed code-along.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json
   end
 
 end
